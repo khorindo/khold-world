529d1a537b7a8549bb9722b62a038b824169e48a
khorindo <rrr.hizon@gmail.com>
2024-12-09 16:14:42 -0600

    completion of topnav for now.

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import MainLayout from "@/components/layout/main_layout";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { Outfit } from "next/font/google";
 
+const outfit = Outfit({ subsets: ["latin"], weight: ["400"] });
 export default function Home() {
   return (
     <MainLayout>
-      <div className="text-center">
+      <div className={`${outfit.className} text-center`}>
         under construction. please watch warmly...
       </div>
     </MainLayout>
