ecdd7e24fc3662ec897a4f6532d57879b6b7e0a2
khorindo <rrr.hizon@gmail.com>
2024-12-13 12:29:51 -0600

    homepage 1.02: sidebar created

--- components/layout/main_layout.js
+++ components/layout/main_layout.js
@@ -1,13 +1,55 @@
 import TopNav from "./top_nav";
+import { Outfit, Ubuntu } from "next/font/google";
+
+import Link from "next/link";
+
+const outfit = Outfit({ subsets: ["latin"], weight: ["400"] });
+const ubuntubold = Ubuntu({ subsets: ["latin"], weight: ["700"] });
 
 export default function MainLayout({ children }) {
   return (
-    <>
-      <header>
-        <TopNav />
+    <main>
+      <header className="w-[900px] m-auto py-2">
+        {/* <TopNav /> */}
+        {/* i dont like the old topnav. i think im going to put the nav ul into the "topnav" component
+        and put that into the div below header where it already is */}
+        <Link href="#">
+          <h1 className={`${ubuntubold.className}  text-4xl`}>khold.world</h1>
+        </Link>
       </header>
-      <main>{children}</main>
+      <div className={`${outfit.className} w-[900px] m-auto`}>
+        <nav
+          className={`${ubuntubold.className}  bg-white border border-b-0 border-black text-black text-2xl`}
+        >
+          <ul className="flex justify-between text-center ">
+            <li className="flex-1 border-r border-black py-1 px-4 hover:bg-black hover:text-white">
+              <a href="#">home</a>
+            </li>
+            <li className="flex-1  border-black p-1 px-4 hover:bg-black hover:text-white">
+              <a href="#">about</a>
+            </li>
+            <li className="flex-1  border-l border-black py-1 px-4 hover:bg-black hover:text-white">
+              <a href="#">blog</a>
+            </li>
+            <li className="flex-1  border-l border-black py-1 px-4 hover:bg-black hover:text-white">
+              <a href="#">fun</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="flex bg-white">
+          {/* New flex container */}
+          <aside className="w-52 border border-black p-4 text-black">
+            {/* Sidebar content */}
+            Sidebar
+          </aside>
+          <div className="flex-1 ">
+            {/* Main content */}
+            {children}
+          </div>
+        </div>
+      </div>
       <footer></footer>
-    </>
+    </main>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
 import MainLayout from "@/components/layout/main_layout";
 
+import { Ubuntu } from "next/font/google";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { Outfit } from "next/font/google";
 
-const outfit = Outfit({ subsets: ["latin"], weight: ["400"] });
+const ubuntubold = Ubuntu({ subsets: ["latin"], weight: ["700"] });
 export default function Home() {
   return (
     <MainLayout>
-      <div className={`${outfit.className} text-center`}>
-        under construction. please watch warmly...
+      <div className="bg-white border-y border-r border-black p-2.5  text-black">
+        <h1 className={`${ubuntubold.className} text-3xl`}>
+          it's a khold world... <span className="text-base">(bundle up.)</span>
+        </h1>
+
+        {/* below here needs to be a flex container so i can put more things here in a nice grid probably 2 x something */}
+
+        <div className="text-center mb-4">
+          under construction. please watch warmly...
+        </div>
       </div>
     </MainLayout>
   );
