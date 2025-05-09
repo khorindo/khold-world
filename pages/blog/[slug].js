82014d4378c9d2b54a8e161cc6f7aa5786e01fc7
khorindo <rrr.hizon@gmail.com>
2025-05-08 19:40:07 -0500

    major rework of blog index/slug.js to use ghost.

---
+++ pages/blog/[slug].js
@@ -0,0 +1,67 @@
+import { useParams } from "next/navigation";
+import { getSinglePost, getBlogPosts } from "@/lib/blog/GetBlogPosts";
+import MainLayout from "@/components/layout/main_layout";
+import Head from "next/head";
+
+// PostPage page component
+const PostPage = (props) => {
+  // Render post title and content in the page from props
+  return (
+    <div>
+      <h1>{props.post.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: props.post.html }} />
+    </div>
+  );
+};
+
+export async function getStaticPaths() {
+  const posts = await getBlogPosts();
+
+  // Get the paths we want to create based on posts
+  const paths = posts.map((post) => ({
+    params: { slug: post.slug },
+  }));
+
+  // { fallback: false } means posts not found should 404.
+  return { paths, fallback: false };
+}
+
+// Pass the page slug over to the "getSinglePost" function
+// In turn passing it to the posts.read() to query the Ghost Content API
+export async function getStaticProps(context) {
+  const post = await getSinglePost(context.params.slug);
+
+  if (!post) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { post },
+  };
+}
+
+export default function BlogPost({ post }) {
+  return (
+    <MainLayout>
+      <Head>
+        <title>{post.title} @ khold.world</title>
+      </Head>
+      <article className="bg-zinc-50 border border-black p-2.5 my-3 mr-2 text-zinc-800 rounded-sm">
+        <header className="mb-4">
+          <h1 className="text-3xl font-bold">{post.title}</h1>
+          <time className="text-sm text-zinc-600">
+            {new Date(post.published_at).toLocaleDateString()}
+          </time>
+        </header>
+        <div
+          className="prose prose-zinc max-w-none  
+          prose-img:rounded-sm 
+          prose-img:mx-auto"
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
+      </article>
+    </MainLayout>
+  );
+}
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,13 +1,29 @@
 // This page will display the existing blog posts
-// next imports
+import MainLayout from "@/components/layout/main_layout";
 import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 // import getBlogPosts function
 import { getBlogPosts } from "../../lib/blog/GetBlogPosts";
+import Head from "next/head";
 
 // fetch requires getStaticProps
 export async function getStaticProps() {
-  const posts = getBlogPosts();
+  const posts = await getBlogPosts();
+
+  if (!posts) {
+    return {
+      notFound: true,
+    };
+  }
+
   return {
     props: { posts },
   };
@@ -15,19 +31,33 @@ export async function getStaticProps() {
 
 export default function BlogIndex({ posts }) {
   return (
-    <div>
-      <h1>Blog</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.slug}>
-            <Link href={`/blog/${post.slug}`}>
-              <h2>{post.title}</h2>
-              <p>{post.description}</p>
-              <small>{new Date(post.date).toLocaleDateString()}</small>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <MainLayout>
+      <Head>
+        <title>blog @ khold.world</title>
+      </Head>
+      <div>
+        <ul className="pt-2">
+          {posts.map((post) => (
+            <li key={post.slug}>
+              <Link href={`/blog/${post.slug}`}>
+                <Card className="bg-zinc-50 border border-black p-2.5 my-1 mr-2 text-zinc-800  rounded-sm ">
+                  <CardHeader>
+                    <CardTitle>{post.title}</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <CardDescription>{post.excerpt}</CardDescription>
+                  </CardContent>
+                  <CardFooter>
+                    <small>
+                      {new Date(post.published_at).toLocaleDateString()}
+                    </small>
+                  </CardFooter>
+                </Card>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </MainLayout>
   );
 }
